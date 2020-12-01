1f776d6a56d4897af51f80dc117818516177555e
Lapsusone <6044855+Lapsusone@users.noreply.github.com>
2020-12-01 13:09:46 -0500

    day 01 done

--- advent2020/day01/fix_expense_report.py
+++ advent2020/day01/fix_expense_report.py
@@ -1,12 +1,32 @@
 import os
 
-lines = (
-    int(x)
-    for x in open(os.path.join(os.path.dirname(__file__), "input_test.txt"))
-    .read()
-    .splitlines()
-)
+dirname = os.path.dirname(__file__)
+lines = [int(x[:-1]) for x in open(os.path.join(dirname, "input.txt"), "r").readlines()]
 
-for a in lines:
-    if (2020 - a) in lines:
-        print((2020 - a) * a)
+
+def part1(input):
+    for a in input:
+        if (2020 - a) in input:
+            return (2020 - a) * a
+
+
+def part2(input):
+    for a in input:
+        for b in input:
+            if (2020 - a - b) in input:
+                return (2020 - a - b) * a * b
+
+
+def test():
+    lines = [
+        int(x[:-1])
+        for x in open(os.path.join(dirname, "input_test.txt"), "r").readlines()
+    ]
+    assert part1(lines) == 514579
+    assert part2(lines) == 241861950
+
+
+if __name__ == "__main__":
+    test()
+    print(f"Part 1: {part1(lines)}")
+    print(f"Part 2: {part2(lines)}")
