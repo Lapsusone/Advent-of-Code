05382c4a9beb6a0ef84a88173fae0bf53d176185
Lapsusone <6044855+Lapsusone@users.noreply.github.com>
2020-12-03 16:41:45 -0500

    day 3 part 2 done

--- advent2020/day03/slope_navigator.py
+++ advent2020/day03/slope_navigator.py
@@ -2,25 +2,39 @@ import os
 
 
 def navigate(input, right, down):
-    lines = input.read().splitlines()
+    lines = input.splitlines()
     height = len(lines)
     width = len(lines[0])
     tree_count = 0
-    i = 1
-    while i < height:
-        index = i * right % width
-        if lines[i - 1 + down][index] == "#":
+    y = 0
+    x = 0
+    while y < height:
+        if lines[y][x] == "#":
             tree_count += 1
-        i += 1
+        x = (x + right) % width
+        y += down
     return tree_count
 
 
+def part2(input):
+    total = 1
+    for right, down in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]:
+        total *= navigate(input, right, down)
+    return total
+
+
 def test():
-    file = open(os.path.join(os.path.dirname(__file__), "input_test.txt"))
+    file = open(os.path.join(os.path.dirname(__file__), "input_test.txt")).read()
     assert navigate(file, 3, 1) == 7
+    assert navigate(file, 1, 1) == 2
+    assert navigate(file, 5, 1) == 3
+    assert navigate(file, 7, 1) == 4
+    assert navigate(file, 1, 2) == 2
+    assert part2(file) == 336
 
 
 if __name__ == "__main__":
     test()
-    file = open(os.path.join(os.path.dirname(__file__), "input.txt"))
+    file = open(os.path.join(os.path.dirname(__file__), "input.txt")).read()
     print(f"Part 1: {navigate(file, 3, 1)}")
+    print(f"Part 2: {part2(file)}")
