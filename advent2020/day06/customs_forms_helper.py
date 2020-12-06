1fdf5a0f286f064f9b8fca393c9dccc5d585205a
Lapsusone <6044855+Lapsusone@users.noreply.github.com>
2020-12-06 18:39:58 -0500

    day 6 completed

--- advent2020/day06/customs_forms_helper.py
+++ advent2020/day06/customs_forms_helper.py
@@ -2,18 +2,27 @@ import os
 
 
 def read_file(filename: str) -> list:
-    file = open(os.path.join(os.path.dirname(__file__), filename)).read().split("\n\n")
-    return [line.replace("\n", "") for line in file]
+    return open(os.path.join(os.path.dirname(__file__), filename)).read().split("\n\n")
 
 
 def part1(groups: list) -> int:
-    return sum([len(set(answers)) for answers in groups])
+    return sum([len(set(answers.replace("\n", ""))) for answers in groups])
+
+
+def part2(groups: list) -> int:
+    group_answers = [answers.splitlines() for answers in groups]
+    for i, answers in enumerate(group_answers):
+        for j, person in enumerate(answers):
+            group_answers[i][j] = set(list(person))
+    return sum(len(set.intersection(*group)) for group in group_answers)
 
 
 def test():
     assert part1(read_file("input_test.txt")) == 11
+    assert part2(read_file("input_test.txt")) == 6
 
 
 if __name__ == "__main__":
     test()
     print("Part 1: ", part1(read_file("input.txt")))
+    print("Part 2: ", part2(read_file("input.txt")))
